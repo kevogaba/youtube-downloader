a66ba289e041a81fd2e41f2f6235a598b10adc4c
Kevin Ogaba <57948471+kevogaba@users.noreply.github.com>
2022-02-01 22:41:13 +0300

    Update download.py

--- download.py
+++ download.py
@@ -7,15 +7,10 @@ try:
 except: 
     print("Connection Error") #to handle exception 
   
-# filters out all the files with "mp4" extension 
-mp4files = yt.filter('mp4') 
-  
-#to set the name of the file
-yt.set_filename('GeeksforGeeks Video')  
-  
 # get the video with the extension and
 # resolution passed in the get() function 
 d_video = yt.get(mp4files[-1].extension,mp4files[-1].resolution) 
+yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download()
 try: 
     # downloading the video 
     d_video.download() 
